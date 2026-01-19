c294c16b9eda5a7d4cf1c5afc805962ef5654a52
Akash <161524961+akash8987@users.noreply.github.com>
2026-01-19 16:52:46 +0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  paths: {
+    sources: "./", 
+    tests: "./",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
